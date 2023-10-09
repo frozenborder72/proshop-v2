dee6561b0da1f006f63636f90c76a57f408a94d8
marcomanunta <frozenborder@gmail.com>
2023-10-09 23:55:29 +0200

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
