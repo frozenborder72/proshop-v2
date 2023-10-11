34dc2047459da3de5796c59ebc03a0d61bf7afcd
marcomanunta <frozenborder@gmail.com>
2023-10-11 20:01:16 +0200

    added Spinner and Message component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
 
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery()
@@ -8,9 +9,11 @@ const HomeScreen = () => {
   return (
     <>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           <h1>Latest Products</h1>
