765e5f887148abcf501676c4e4584d21441f1ec8
marcomanunta <frozenborder@gmail.com>
2023-10-14 20:24:44 +0200

    completed checkout process

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import store from './store.js'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/bootstrap.custom.css'
@@ -18,6 +19,12 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
+import ShippingScreen from './screens/ShippingScreen.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import PaymentScreen from './screens/PaymentScreen.jsx'
+import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx'
+import OrderScreen from './screens/OrderScreen.jsx'
+import ProfileScreen from './screens/ProfileScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +34,14 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 )
@@ -35,7 +50,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 )
--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -2,19 +2,19 @@ export const addDecimal = num => (Math.round(num * 100) / 100).toFixed(2)
 
 export const updateCart = state => {
   // Calculate items price
-  state.itemPrice = addDecimal(
+  state.itemsPrice = addDecimal(
     state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   )
 
   // Calculate shipping price (if order > $100 free, else $10 shipping)
-  state.shippingPrice = addDecimal(state.itemPrice > 100 ? 0 : 10)
+  state.shippingPrice = addDecimal(state.itemsPrice > 100 ? 0 : 10)
 
   // Calculate tax price (15%)
-  state.taxPrice = addDecimal(Number(0.15 * state.itemPrice).toFixed(2))
+  state.taxPrice = addDecimal(Number(0.15 * state.itemsPrice).toFixed(2))
 
   // Calculate total price
   state.totalPrice = (
-    Number(state.itemPrice) +
+    Number(state.itemsPrice) +
     Number(state.shippingPrice) +
     Number(state.taxPrice)
   ).toFixed(2)
